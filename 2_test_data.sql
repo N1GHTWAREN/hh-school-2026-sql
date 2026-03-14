7e89edb9ba7f4de7402582ac52b22dafc892a5d6
Esipov Bogdan <bogdanesipov06@mail.ru>
2026-03-14 22:37:14 +0300

    Сделал поправки в дз

--- 2_test_data.sql
+++ 2_test_data.sql
@@ -1,50 +1,50 @@
-WITH test_data AS (SELECT generate_series(1, 1000000) AS id,
-    					  md5(random()::text) AS name,
+WITH test_data AS (SELECT md5(random()::text) AS name,
+						  (random()*200000)::int AS income,
+						  random() > 0.5 AS edu
+				   FROM generate_series(1, 1000))
+INSERT INTO specializations (specialization_name,
+							 avg_income_level,
+							 education)
+SELECT name, income, edu
+FROM test_data;
+
+WITH test_data AS (SELECT md5(random()::text) AS name,
 						  (random() * 100)::int AS a_id,
 						  round((random() * 100000)::int) AS c_from,
-						  (random() * 1000)::int AS s_id,
-						  '2023-01-01'::timestamp + ('2026-01-01'::timestamp - '2023-01-01'::timestamp) * random() AS dat)
-INSERT INTO vacancies (vacancy_id,
-    				   vacancy_name,
+						  floor(random() * (SELECT max(specialization_id) FROM specializations))::int + 1 AS s_id,
+						  '2023-01-01'::timestamp + ('2026-01-01'::timestamp - '2023-01-01'::timestamp) * random() AS dat
+				   FROM generate_series(1, 1000000))
+INSERT INTO vacancies (vacancy_name,
 					   area_id,
 					   compensation_from,
 					   compensation_to,
 					   specialization_id,
 					   date_of_public)
-SELECT id, name, a_id, c_from, c_from + 10000, s_id, dat
+SELECT name, a_id, c_from, c_from + 10000, s_id, dat
 FROM test_data;
 
-WITH test_data AS (SELECT generate_series(1, 1000000) AS id,
-						  md5(random()::text) AS name,
+WITH test_data AS (SELECT md5(random()::text) AS name,
 						  (random() * 10)::int AS exp,
 						  round((random() * 100000)::int) AS income,
-						  (random() * 1000)::int AS s_id,
+						  floor(random() * (SELECT max(specialization_id) FROM specializations))::int + 1 AS s_id,
 						  (random() * 1000)::int AS c_id,
-						  '2023-01-01'::timestamp + ('2026-01-01'::timestamp - '2023-01-01'::timestamp) * random() AS dat)
-INSERT INTO resumes (resume_id,
-    				 resume_name,
+						  '2023-01-01'::timestamp + ('2026-01-01'::timestamp - '2023-01-01'::timestamp) * random() AS dat
+				   FROM generate_series(1, 1000000))
+INSERT INTO resumes (resume_name,
 					 experience,
 					 income_level,
 					 specialization_id,
 					 last_company_id,
 					 date_of_public)
-SELECT id, name, exp, income, s_id, c_id, dat
+SELECT name, exp, income, s_id, c_id, dat
 FROM test_data;
 
-INSERT INTO responces (responce_id,
-    				   vacancy_id,
+WITH test_data AS (SELECT floor(random() * (SELECT max(vacancy_id) FROM vacancies))::int + 1 AS v_id,
+        				  floor(random() * (SELECT max(resume_id) FROM resumes))::int + 1 AS r_id,
+						  '2023-01-01'::timestamp + ('2026-01-01'::timestamp - '2023-01-01'::timestamp) * random() AS dat
+				   FROM generate_series(1, 1000000))
+INSERT INTO responces (vacancy_id,
 					   resume_id,
 					   date_of_responce)
-SELECT row_number() OVER () AS responce_id,
-	   vac.vacancy_id,
-	   (random() * 1000000)::int + 1,
-	   vac.date_of_public + (random() * interval '60 days')
-FROM vacancies vac
-CROSS JOIN LATERAL generate_series(1, (random() * 50)::int);
-
-UPDATE vacancies vac
-SET responce_ids = r.ids
-FROM (SELECT vacancy_id,
-        	 array_agg(responce_id ORDER BY responce_id) AS ids
-      FROM responces GROUP BY vacancy_id) res
-	  WHERE vac.vacancy_id = res.vacancy_id;
+SELECT v_id, r_id, dat
+FROM test_data;
